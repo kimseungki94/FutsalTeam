4ac4346ef2f16824fe0ce727b24e3ebc77f7275e
kimseungki94 <skkim0832@gmail.com>
2018-11-21 20:51:46 +0900

    group homepage processing

---
+++ activate/groupfunction.js
@@ -0,0 +1,20 @@
+
+
+const me = {
+
+    account: [
+        {
+    name : "kim",
+    age : "25",
+    gender:"Male",
+    Handsome: "true"
+        }
+    ],
+    hobby: [
+        {
+            some : "soccer"
+        }
+    ]
+}
+console.log(me);
+alert('Im ng');
